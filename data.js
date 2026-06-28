41dc8dc8b8f98f86e690e920ee04a5ee301bee50
JeslyDesign <jeslyking85@gmail.com>
2026-06-28 14:38:15 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom-view1",
+      "name": "Bedroom View1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5729459719646854,
+          "pitch": 0.42253543056946796,
+          "rotation": 4.71238898038469,
+          "target": "1-bedroom-view2"
+        },
+        {
+          "yaw": -0.6950724346995649,
+          "pitch": 0.319744506961829,
+          "rotation": 0.7853981633974483,
+          "target": "2-bedroom-view3"
+        },
+        {
+          "yaw": 1.5461150225161227,
+          "pitch": 0.5269832336172104,
+          "rotation": 5.497787143782138,
+          "target": "3-bedroom-view4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom-view2",
+      "name": "Bedroom View2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8447463113831848,
+          "pitch": 0.4700561807283936,
+          "rotation": 0,
+          "target": "0-bedroom-view1"
+        },
+        {
+          "yaw": -0.9407475721752832,
+          "pitch": 0.4165039361431173,
+          "rotation": 0.7853981633974483,
+          "target": "2-bedroom-view3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom-view3",
+      "name": "Bedroom View3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0654429563666348,
+          "pitch": 0.2528481353584269,
+          "rotation": 0,
+          "target": "1-bedroom-view2"
+        },
+        {
+          "yaw": 2.039391086868739,
+          "pitch": 0.43001288523245407,
+          "rotation": 5.497787143782138,
+          "target": "0-bedroom-view1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom-view4",
+      "name": "Bedroom View4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0138813500011956,
+          "pitch": 0.521508164351328,
+          "rotation": 0.7853981633974483,
+          "target": "0-bedroom-view1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
